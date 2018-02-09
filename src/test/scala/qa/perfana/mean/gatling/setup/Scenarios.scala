703a8807b20bcab3026c870008b5e4f4b60f0d69
Daniel Moll <dmoll1974@gmail.com>
2018-02-09 17:07:01 +0100

    exercise 1 result

---
+++ src/test/scala/qa/perfana/mean/gatling/setup/Scenarios.scala
@@ -0,0 +1,39 @@
+package qa.perfana.mean.gatling.setup
+
+import qa.perfana.mean.gatling.useCases._
+import qa.perfana.mean.gatling.feeders._
+import io.gatling.core.Predef._
+import scala.concurrent.duration._
+
+/**
+ * This object collects the Scenarios in the project for use in the Simulation. There are two
+ * main properties in this object: acceptanceTestScenario and debugScenario. These two are
+ * used in the Simulation class to setup the actual tests to run. If you wish to add
+ * scenarios to either run, add them here. 
+ */
+object Scenarios {
+
+  /**
+   * These are the scenarios run in 'normal' mode.
+   */
+  val acceptanceTestScenario = scenario("Acceptance test").feed(SomeDataFeeder.someData)
+    .exitBlockOnFail(
+      randomSwitch(
+
+        95.0 -> exec(UseCase2.steps),
+        5.0 -> exec(UseCase1.steps)
+
+      )
+    )
+
+  /**
+   * These are the scenarios run in 'debug' mode.
+   */
+  val debugScenario = scenario("debug").feed(SomeDataFeeder.someData)
+    .exitBlockOnFail(
+      exec(UseCase2.steps)
+      .exec(UseCase1.steps)
+    )
+
+
+}
