1255813a0b7bd5fa9c4659721dbbaf91e5abdeb8
Daniel Moll <dmoll1974@gmail.com>
2019-01-30 13:00:01 +0100

    added pipeline file

---
+++ perfana-jenkins-demo.groovy
@@ -0,0 +1,25 @@
+node("master") {
+
+    def testRunId = env.JOB_NAME + "-" + env.BUILD_NUMBER
+    def buildUrl = env.BUILD_URL
+
+    // Mark the code checkout 'stage'....
+
+    stage 'Checkout'
+
+    // Get script from a GitHub repository
+    git url: 'https://github.com/perfana/perfana-gatling-workshop.git'
+    // Get the maven tool.
+    // ** NOTE: This 'M3' maven tool must be configured
+    // **       in the global configuration.
+    def mvnHome = tool 'M3'
+
+
+    stage 'Execute load test'
+
+    // Run the test
+    sh "${mvnHome}/bin/mvn clean install perfana-gatling:test -Ptest-env-acc,test-type-load,assert-results -DtestRunId=$testRunId -DbuildResultsUrl=$buildUrl"
+
+
+
+}
