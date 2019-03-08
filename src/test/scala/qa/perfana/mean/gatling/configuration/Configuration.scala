f32ced88e71703b186fbe62efc434504f2e073af
Daniel Moll <dmoll1974@gmail.com>
2019-03-08 10:25:40 +0100

    added application and test env to graphite prefix

--- src/test/scala/qa/perfana/mean/gatling/configuration/Configuration.scala
+++ src/test/scala/qa/perfana/mean/gatling/configuration/Configuration.scala
@@ -70,6 +70,7 @@ object Configuration {
     .userAgentHeader("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36")
     .inferHtmlResources(WhiteList(""".*""" + Configuration.targetBaseUrl + """.*"""))
     .silentResources
+    .disableWarmUp
 
 
 
